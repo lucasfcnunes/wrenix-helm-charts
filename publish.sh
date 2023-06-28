5b4621dc447c46e86a9994d007d1fba1edee30c6
WrenIX <dev@wrenix.eu>
2023-06-29 00:53:48 +0200

    fix(ci): add publish script with lint, tag and helm package+push

---
+++ publish.sh
@@ -0,0 +1,48 @@
+#/bin/sh
+
+HELM_REPO=${HELM_REPO:-oci://codeberg.org/wrenix/helm-charts}
+
+for p in * ; do
+  if [ ! -d $p ]; then
+    continue;
+  fi
+
+  echo "${p}:";
+
+  # last pkg  
+  lastTag=$(git tag -l "${p}-v*" | tail -1)
+  lastVersion=${lastTag#${p}-v}
+  echo "last pkg: ${lastVersion}"
+
+  # Chart version
+  v=$(dasel -f "${p}/Chart.yaml" -s version)
+  echo "version: ${v}";
+
+  #  should be increased?
+  tag="${p}-v${v}"
+  # check if a old version exists
+  if [ -n $lastTag ]; then
+  
+    # check if changes since new version happen
+    changes=$(git diff "${lastTag}" -- "${p}" | wc -l);
+    if [ "$changes" -gt "0" ]; then
+      # check / lint if version was increased correct
+      if [ "$tag" == "$lastTag" ]; then
+        echo "changed helmchart should create new pkg"
+        echo $(git diff "${lastTag}" -- "${p}" | wc -l);
+        exit 1
+      fi
+    else
+      echo "nothing todo"
+      echo
+      continue;
+    fi
+  fi
+  helm package "${p}"
+  helm push "${p}-${v}.tgz" "${HELM_REPO}";
+
+  git tag "${tag}" --no-sign;
+  git push --tags;
+
+  echo
+done

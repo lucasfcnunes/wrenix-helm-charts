b41a2a9f45ef9e4dee16199d5bee9e0d1b5efc2f
WrenIX <dev@wrenix.eu>
2024-02-13 19:47:19 +0100

    fix(postgresql): init job correct quote

--- postgresql/files/10-init-user.sh
+++ postgresql/files/10-init-user.sh
@@ -1,10 +1,10 @@
 {{- range $username, $password := .Values.job.users }}
-echo 'user "{{ $username }}":'
-psql -tc "SELECT 1 FROM pg_user WHERE usename = '{{ $username }}'" | grep -q 1;
+echo 'user {{ $username | quote }}:'
+psql -tc "SELECT 1 FROM pg_user WHERE usename = {{ $username | squote }}" | grep -q 1;
 if [ $? -ne 0 ]; then
-  psql -c "CREATE USER {{ $username }} WITH ENCRYPTED PASSWORD '{{ $password }}'";
+  psql -c "CREATE USER \"{{ $username }}\" WITH ENCRYPTED PASSWORD {{ $password | squote }}";
 else
-  psql -c "ALTER USER {{ $username }} WITH ENCRYPTED PASSWORD '{{ $password }}'";
+  psql -c "ALTER USER \"{{ $username }}\" WITH ENCRYPTED PASSWORD {{ $password | squote }}";
 fi
 echo ""
 {{- end }}
--- postgresql/files/20-init-db.sh
+++ postgresql/files/20-init-db.sh
@@ -1,10 +1,10 @@
 {{- range $name, $config := .Values.job.databases }}
-echo 'database "{{ $name }}":'
-psql -tc "SELECT 1 FROM pg_database WHERE datname = '{{ $name }}'" | grep -q 1
+echo 'database {{ $name | quote }}:'
+psql -tc "SELECT 1 FROM pg_database WHERE datname = {{ $name | squote }}" | grep -q 1
 if [ $? -ne 0 ]; then
-  psql -c "CREATE DATABASE {{ $name }} {{ with $config.additionalParams }}{{ . }} {{ end }}";
+  psql -c 'CREATE DATABASE {{ $name | quote }} {{ with $config.additionalParams }}{{ . }} {{ end }}';
 fi
-psql -c "ALTER DATABASE {{ $name }} OWNER TO {{ $config.owner }}";
-psql -c "GRANT ALL PRIVILEGES ON DATABASE {{ $name }} TO {{ $config.owner }}";
+psql -c 'ALTER DATABASE {{ $name | quote }} OWNER TO {{ $config.owner | quote }}';
+psql -c 'GRANT ALL PRIVILEGES ON DATABASE {{ $name | quote }} TO {{ $config.owner | quote }}';
 echo ""
 {{- end }}
